fae99b3af3a3fe04df419a6beb8103bc2ddb2cd0
EPAM\Iryna_Shyrshova <Iryna_Shyrshova@epam.com>
2024-07-07 17:10:51 +0300

    feat: Implement module1 RxJS-in-Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
   combineLatest,
+  debounceTime,
+  distinctUntilChanged,
   filter,
   forkJoin,
   map,
@@ -17,6 +19,7 @@ import { MockDataService } from './mock-data.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
+  currentSearchTerm: string = '';
   searchTermByCharacters = new Subject<string>();
   charactersResults$!: Observable<any>;
   planetAndCharactersResults$!: Observable<any>;
@@ -33,47 +36,53 @@ export class AppComponent implements OnInit, OnDestroy {
   changeCharactersInput(element: any): void {
     // 1.1. Add functionality to changeCharactersInput method. Changes searchTermByCharacters Subject value on input change.
     const inputValue: string = element.target.value;
-    // YOUR CODE STARTS HERE
-
-    // YOUR CODE ENDS HERE
+    this.currentSearchTerm = inputValue;
+    this.searchTermByCharacters.next(inputValue);
   }
 
   initCharacterEvents(): void {
     // 1.2. Add API call on each user input. Use mockDataService.getCharacters - to make get request.
-
     // 2. Since we don't want to spam our service add filter by input value and do not call API until a user enters at least 3 chars.
-
     // 3. Add debounce to prevent API calls until user stop typing.
-
     this.charactersResults$ = this.searchTermByCharacters
-        .pipe
-        // YOUR CODE STARTS HERE
-
-        // YOUR CODE ENDS HERE
-        ();
+        .pipe(
+          map((query: string) => (query ? query.trim() : "")),
+          filter((query: string) => query.length >= 3),
+          debounceTime(500),
+          distinctUntilChanged(),
+          switchMap((query: string) => this.mockDataService.getCharacters(query))
+        );
   }
 
   loadCharactersAndPlanet(): void {
     // 4. On clicking the button 'Load Characters And Planets', it is necessary to process two requests and combine the results of both requests into one result array. As a result, a list with the names of the characters and the names of the planets is displayed on the screen.
     // Your code should looks like this: this.planetAndCharactersResults$ = /* Your code */
-    // YOUR CODE STARTS HERE
-    // YOUR CODE ENDS HERE
+    this.planetAndCharactersResults$ = forkJoin([
+      this.mockDataService.getPlanets(this.currentSearchTerm),
+      this.mockDataService.getCharacters(this.currentSearchTerm)
+    ])
+    .pipe(map(([planets, characters]) => [...characters, ...planets]));
   }
 
   initLoadingState(): void {
     /* 5.1. Let's add loader logic to our page. For each request, we have an observable that contains the state of the request. When we send a request the value is true, when the request is completed, the value becomes false. You can get value data with mockDataService.getCharactersLoader() and mockDataService.getPlanetLoader().
-
     - Combine the value of each of the streams.
     - Subscribe to changes
     - Check the received value using the areAllValuesTrue function and pass them to the isLoading variable. */
-    // YOUR CODE STARTS HERE
-    // YOUR CODE ENDS HERE
+    const characterLoader$ = this.mockDataService.getCharactersLoader();
+    const planetLoader$ = this.mockDataService.getPlanetLoader();
+    const combineLoader$ = combineLatest([characterLoader$, planetLoader$]);
+
+    const loadingSubscription = combineLoader$.subscribe(([characterLoading, planetLoading]) => {
+      this.isLoading = this.areAllValuesTrue([characterLoading, planetLoading]);
+    });
+
+    this.subscriptions.push(loadingSubscription);
   }
 
   ngOnDestroy(): void {
     // 5.2 Unsubscribe from all subscriptions
-    // YOUR CODE STARTS HERE
-    // YOUR CODE ENDS HERE
+    this.subscriptions.forEach((subscriptions) => subscriptions.unsubscribe());
   }
 
   areAllValuesTrue(elements: boolean[]): boolean {
